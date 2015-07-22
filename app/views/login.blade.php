42bb5c871a43179ad0d890fe3a6e83ba2348c58d
Brandon Zeek <zkbrndn@yahoo.com>
2015-07-21 20:47:48 -0500

    login page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
   <div class="wrapper">
-{{ Form::open(array('url' => 'login')) }}
-<h1>Login</h1>
+{{ Form::open(array('url' => 'login', 'class' => 'form-signin')) }}
+<h2 class="form-signin-heading">Please login</h2>
 @if(Session::has('error'))
 <div class="alert-box success">
   <h2>{{ Session::get('error') }}</h2>
@@ -20,7 +21,7 @@
 {{ Form::password('password',array('class'=>'form-control span6', 'placeholder' => 'Please Enter your Password')) }}
 <p class="errors">{{$errors->first('password')}}</p>
 </div>
-<p>{{ Form::submit('Login', array('class'=>'send-btn')) }}</p>
+<p>{{ Form::submit('Login', array('class'=>'btn btn-lg btn-primary btn-block')) }}</p>
 {{ Form::close() }}
 
 </div>
@@ -32,6 +33,36 @@ body {
   margin-top: 80px;
   margin-bottom: 80px;
 }
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);
+
+  .form-signin-heading,
+	.checkbox {
+	  margin-bottom: 30px;
+	}
+
+	.form-control span6 {
+	  position: relative;
+	  font-size: 16px;
+	  height: auto;
+	  padding: 10px;
+	}
+
+  input[type="text"] {
+	  margin-bottom: -1px;
+	  border-bottom-left-radius: 0;
+	  border-bottom-right-radius: 0;
+	}
+
+	input[type="password"] {
+	  margin-bottom: 20px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
 </style>
 </body>
 </html>
